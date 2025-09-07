715beadd100cf9494cb35b9b2611692a990077cf
zebz13 <zeb@fedora>
2025-09-07 18:13:59 +0530

    Using list and dict comprehension instead of for loop

--- solution.py
+++ solution.py
@@ -4,16 +4,20 @@ class Solution:
     def minSteps(self, s: str, t: str) -> int:
         d = {}
         count = 0
-        for c in s:
-            d[c] = 1 if not d.get(c) else d[c] + 1
+        d = {x: s.count(x) for x in set(s)}
+        # for c in s:
+        #     d[c] = 1 if not d.get(c) else d[c] + 1
 
-        for c in t:
-            if d.get(c):
-                d[c] -= 1
-            else:
-                count += 1
+
+        count = sum([max(0, t.count(x)-d.get(x, 0)) for x in set(t)])
+        # for c in t:
+        #     if d.get(c):
+        #         d[c] -= 1
+        #     else:
+        #         count += 1
 
         return count
+
 
 # Test cases
 test_cases = [
